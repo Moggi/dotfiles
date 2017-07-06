b694d4f992076827c29addd992be129e9e639fc9
Paulo Moggi <paulohpmoggi@gmail.com>
2017-07-06 10:14:20 -0300

    mac stuff

---
+++ homebrew.sh
@@ -0,0 +1,22 @@
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install zsh
+brew install git
+brew install vim
+brew install curl
+brew install wget
+brew install nmap		#YES!
+brew install rcm
+brew install docker
+brew install docker-machine
+brew install tmux
+
+brew install jq			#you never know when..
+brew install htop
+brew install figlet		#yeah
+
+brew install python3
+brew install node
+brew install go
+brew install mono
+brew install sqlite
+
